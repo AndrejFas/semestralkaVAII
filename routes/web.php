6d25601bef0302053e24bf7f9e5feec8ccf048f7
fasanok <fasanokandrej@gmail.com>
2023-12-05 13:41:16 +0100

    fungujuce admin

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ Route::get('/pridaj', [YourController::class, 'pridaj'])->name('pridaj');
 Route::post('/pridaj-pouzivatela', [PouzivatelController::class, 'pridajPouzivatela'])->name('pridaj.pouzivatela');
 Route::get('/pridajUser', function(){return view('addPouzivatel');})->name('pridajuser');
 
-Route::get('/zobraz-uzivatele', [PouzivatelController::class, 'zobrazUzivatele'])->name('zobrazUzivatele');
 Route::delete('/odstran-uzivatele/{id}', [PouzivatelController::class, 'odstranUzivatele'])->name('odstranUzivatele');
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/zobraz-uzivatele', [PouzivatelController::class, 'zobrazUzivatele'])->name('zobrazUzivatele');
+});
